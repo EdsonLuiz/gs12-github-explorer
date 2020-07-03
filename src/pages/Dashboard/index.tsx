9fe8c6ca12fb3d1e28f0413f8f414512436f1fba
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-07-02 21:20:38 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import {FiChevronRight} from 'react-icons/fi'
 
-const Dashboard = () => {
+
+import logoImg from '../../assets/logo.svg'
+
+import {Title, Form, Repositories} from './styles'
+
+const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>     
-    </div>
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Explore repositórios no github.</Title>     
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/5461553?s=460&u=f0ebe858f9b8868943d86fccbcaeafcaf7c66462&v=4" alt="Image of repository owner"/>
+
+          <div>
+            <strong>Edson Luiz</strong>
+            <p>Some description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/5461553?s=460&u=f0ebe858f9b8868943d86fccbcaeafcaf7c66462&v=4" alt="Image of repository owner"/>
+
+          <div>
+            <strong>Edson Luiz</strong>
+            <p>Some description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars3.githubusercontent.com/u/5461553?s=460&u=f0ebe858f9b8868943d86fccbcaeafcaf7c66462&v=4" alt="Image of repository owner"/>
+
+          <div>
+            <strong>Edson Luiz</strong>
+            <p>Some description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   )
 }
 
