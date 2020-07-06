5cca4196464f92de12bfbfe004fb83aa30fdded9
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-07-06 16:39:43 -0300

    Estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import {useRouteMatch} from 'react-router-dom'
+import {useRouteMatch, Link} from 'react-router-dom'
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
+
+import {Header, RepositoryInfo, Issues} from './styles'
+import logoImg from '../../assets/logo.svg'
 
 interface RepositoryParams {
   repository: string
@@ -8,9 +12,49 @@ interface RepositoryParams {
 const Repository = () => {
   const {params} = useRouteMatch<RepositoryParams>()
   return (
-    <div>
-      <h1>Repository: {params.repository}</h1>
-    </div>
+    <>
+      <Header>
+        <img src={logoImg} alt="github explorer"/>
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/583231?v=4" alt="avatar"/>
+          <div>
+            <strong>Octocat/Octocat</strong>
+            <p>Descrição do repositório.</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdfasdf">
+          <div>
+            <strong>Repo_full_name</strong>
+            <p>Description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>       
+      </Issues>
+    </>
   )
 }
 
